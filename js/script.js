4f049688dd9606f9f6ac8e42028bb8930792caec
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-06 15:33:56 +0100

    add: create array of workers

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const workers = [
+  {
+    imgWorker:"wayne-barnett-founder-ceo.jpg",
+    name:"Wayne Barnett",
+    assignment: "Founder & CEO",
+  },
+  {
+    imgWorker:"angela-caroll-chief-editor.jpg",
+    name:"Angela Caroll",
+    assignment: "Chief Editor",
+  },
+  {
+    imgWorker:"walter-gordon-office-manager.jpg",
+    name:"Walter Gordon",
+    assignment: "Office Manager",
+  },
+  {
+    imgWorker:"angela-lopez-social-media-manager.jpg",
+    name:"Angela Lopez",
+    assignment: "Social Media Manager",
+  },
+  {
+    imgWorker:"scott-estrada-developer.jpg",
+    name:"Scott Estrada",
+    assignment: "Developer",
+  },
+  {
+    imgWorker:"barbara-ramos-graphic-designer.jpg",
+    name:"Barbara Ramos",
+    assignment: "Graphic Designer",
+  },
+]
